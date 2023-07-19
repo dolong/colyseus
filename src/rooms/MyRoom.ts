536b0df83f0d5bb052a897903259f69d743a9b3f
dolong <dolong@gmail.com>
2023-07-18 21:33:01 -0400

    room changes

--- src/rooms/MyRoom.ts
+++ src/rooms/MyRoom.ts
@@ -7,10 +7,16 @@ export class MyRoom extends Room<MyRoomState> {
   onCreate (options: any) {
     this.setState(new MyRoomState());
 
-    this.onMessage("type", (client, message) => {
-      //
-      // handle "type" message
-      //
+      //when a message is received of type "message," broadcast it with the type "server-message" to all clients
+      this.onMessage("message", (client, message) => {
+        console.log("Game Room received message from", client.sessionId, ":", message);
+        this.broadcast("server-message", `(${client.sessionId} ${message}`);
+    });
+
+
+    //when a message is received of type "game-message," broadcast it with the type "game-message" to all clients except for the one that sent it
+    this.onMessage("game-message", (client, message) => {
+        this.broadcast("game-message", message, { except: client });
     });
   }
 
